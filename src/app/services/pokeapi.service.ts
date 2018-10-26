3e75bbb5f381255800600f0ae140e2e4ed4ef59a
Antoine Poivey <antoine.poivey@gmail.com>
2018-10-26 09:25:16 +0200

    added pokemon number and pokemon generation to pokemon component

--- src/app/services/pokeapi.service.ts
+++ src/app/services/pokeapi.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import {forkJoin, Observable} from 'rxjs';
@@ -9,7 +9,10 @@ import {NgxSpinnerService} from 'ngx-spinner';
 })
 export class PokeapiService {
 
-  constructor(private _http: HttpClient) { }
+  private _pokemonCountByGeneration = [151, 251, 386, 493, 649, 721, 807];
+
+  constructor(private _http: HttpClient) {
+  }
 
   getPokemons(): Observable<Array<any>> {
     return this.getRangePokemons(1, 30);
@@ -29,6 +32,18 @@ export class PokeapiService {
 
   getPokemonByName(name: string): Observable<any> {
     return this._http.get(`https://pokeapi.co/api/v2/pokemon/${name}/`);
+  }
+
+  getPokemonGeneration(pokemonNumber: number): number {
+    let generation = 1; // Count generation while iterating on array
+    for (const pokemonCount of this._pokemonCountByGeneration) {
+      if (pokemonNumber <= pokemonCount) {
+        return generation;
+      }
+      generation += 1;
+    }
+    // If no generation was returned yet, return generation 0 (generation undetermined)
+    return 0;
   }
 
   callUrl(url: string) {
