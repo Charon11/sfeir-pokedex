32bb1ed7999174c35567c1ac6fb410cd899a0301
Renaud Chardin <chardin.r@sfeir.com>
2024-06-19 12:06:46 +0200

    feat: remove unused config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,13 +2,4 @@ export const environment = {
   production: true,
   limitPokemon: 721,
   requestLimit: 30,
-  firebase: {
-    apiKey: "AIzaSyB0_3hx0T8gIo7t4RVPC-2yLXO3FoqKQb4",
-    authDomain: "raif-pokedex.firebaseapp.com",
-    projectId: "raif-pokedex",
-    storageBucket: "raif-pokedex.appspot.com",
-    messagingSenderId: "907687156541",
-    appId: "1:907687156541:web:9d1a6e32a9db3090954354",
-    vapidKey: 'Rmm1oh0xlcmx2COyl-5kQ9eBishFuOiUOvachkvDw3Q'
-  },
 };
